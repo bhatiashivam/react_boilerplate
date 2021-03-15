02f904137246c6925fe93972d5492be67dbb365c
Shivam Bhatia <bhatiashivam59@gmail.com>
2021-03-15 17:47:25 +0000

    setup eslint prettier airbnb style

--- react-boilerplate/src/components/Nav/Nav.component.jsx
+++ react-boilerplate/src/components/Nav/Nav.component.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-
-import { Link } from "react-router-dom";
-
-import "./Nav.styles.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.styles.css';
 
 const Nav = () => {
   return (
